eee47c4cf2752f83e1ed82514fc6210e47418141
timzz71 <superhacker112323@gmail.com>
2026-07-12 15:03:34 +0300

    Simplify banner output in TimzerzDetector.ps1
    Removed the custom banner message and adjusted the color.

--- TimzerzDetector.ps1
+++ TimzerzDetector.ps1
@@ -29,13 +29,7 @@ $Banner = @"
 "@
 
 Write-Host $Banner -ForegroundColor Cyan
-Write-Host ""
-Write-Host "                Made with " -ForegroundColor Gray -NoNewline
-Write-Host "♥ " -ForegroundColor Red -NoNewline
-Write-Host "by " -ForegroundColor Gray -NoNewline
-Write-Host "Timerzz" -ForegroundColor Cyan
-Write-Host ""
-Write-Host ("━" * 76) -ForegroundColor DarkCyan
+Write-Host ("━" * 76) -ForegroundColor Cyan
 Write-Host
 
 Write-Host "Enter path to the mods folder: " -NoNewline
